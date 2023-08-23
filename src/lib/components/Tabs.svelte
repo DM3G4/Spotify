81bbccfb66f78cdbe2456e6dc1a5e150e588c0b9
DM3G4 <diegom3g4@gmail.com>
2023-08-22 21:40:56 -0600

    Publish to gh pages

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let tabs: Array<string> = [];
+	import { base } from '$app/paths';
 </script>
 
 <div class="text-sm font-medium text-center text-gray-500 flex justify-center">
@@ -8,7 +9,7 @@
 			{#if i === 1}
 				<li class="mr-2" aria-posinset={i + 1}>
 					<a
-						href="/"
+						href="{base}/"
 						class="inline-block p-4 text-green-600 border-b-2 border-green-600 rounded-t-lg active dark:text-green-500 dark:border-green-500"
 						aria-current="page">{tab}</a
 					>
@@ -16,7 +17,7 @@
 			{:else}
 				<li class="mr-2" aria-posinset={i + 1}>
 					<a
-						href="/"
+						href="{base}/"
 						class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-neutral-600 hover:border-neutral-300 dark:hover:text-neutral-300"
 					>
 						{tab}
